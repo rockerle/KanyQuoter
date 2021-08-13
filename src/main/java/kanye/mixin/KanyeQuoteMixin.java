72ad9089a772fd94a8a7edc2b881661aa541afd2
Alex <aschoenit@rhrk.uni-kl.de>
2021-08-13 18:38:42 +0200

    fixed client freezing while getting quotes

--- src/main/java/kanye/mixin/KanyeQuoteMixin.java
+++ src/main/java/kanye/mixin/KanyeQuoteMixin.java
@@ -8,34 +8,40 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Mixin(ClientPlayerEntity.class)
 public class KanyeQuoteMixin {
 
+    private Quotes kq;
+
+    @Inject(at=@At("TAIL"),method="<init>")
+    public void ClientPlayerEntity(CallbackInfo ci){
+        kq = new Quotes("http://api.kanye.rest");
+    }
+
     @Inject( at = @At("HEAD"), method = "sendChatMessage", cancellable = true)
     public void onChatMessage(String message, CallbackInfo ci){
 
         if(message.equals(".kanye")){
-            ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
-            Quotes kq = new Quotes("http://api.kanye.rest");
-            List<String> pages = new ArrayList<>();
+            kq.getQuotes(50);
+            ci.cancel();
+        }
+    }
 
-            try {
-                for(int i=0;i<50;i++)
-                    pages.add(kq.getContent());
-            } catch(Exception e){
-                ci.cancel();
-                return;
-            }
+    @Inject(at = @At("TAIL"), method = "tick", cancellable = true)
+    public void onTick(CallbackInfo ci) {
+
+        if (kq.readyToRead) {
+            ClientPlayerEntity player = (ClientPlayerEntity) (Object) this;
+            List<String> pages = kq.getPages();
 
             if (player.getInventory().getMainHandStack().getItem() == Items.WRITABLE_BOOK)
                 player.networkHandler.sendPacket(new BookUpdateC2SPacket(player.getInventory().selectedSlot, pages, Optional.of("Book of Endless Wisdom")));
 
-            ci.cancel();
+            kq.readyToRead = false;
+            kq.emptyPages();
         }
     }
 }
